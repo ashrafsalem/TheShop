e11bbc7e5cfad1f11e9a57033edddce4690d3331
ashraf adly <ashrafadly@ashrafs-MacBook-Pro.local>
2022-09-17 23:39:49 +0200

    finish till display product items

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs';
 import { IBrand } from '../shared/models/brand';
+import { IProduct } from '../shared/models/product';
 import { IPagination } from '../shared/models/pagination';
 import { IType } from '../shared/models/productType';
 import { ShopParams } from '../shared/models/shopParams';
@@ -39,6 +40,10 @@ export class ShopService {
       })
       
     );
+  }
+
+  getPro(id: number){
+    return this.http.get<IProduct>(this.baseUrl +"products/ "+ id);
   }
 
   getBrands(){
